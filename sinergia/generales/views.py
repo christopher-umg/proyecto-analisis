2487461142a3dd10cee170606b93a64dc4388d6b
Christopher Taquiej <christophertaquiej@gmail.com>
2023-10-23 03:20:43 -0600

    add general apis

--- sinergia/generales/views.py
+++ sinergia/generales/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Pais, Departamento, Municipio
+from .serializers import PaisSerializer, DepartamentoSerializer, DepartamentoDetailSerializer, MunicipioSerializer, MunicipioDetailSerializer
 
-# Create your views here.
+class PaisViewSet(viewsets.ModelViewSet):
+    queryset = Pais.objects.all()
+    serializer_class = PaisSerializer
+
+class DepartamentoViewSet(viewsets.ModelViewSet):
+    queryset = Departamento.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == 'retrieve' or self.action == 'list':
+            return DepartamentoDetailSerializer
+        return DepartamentoSerializer
+
+class MunicipioViewSet(viewsets.ModelViewSet):
+    queryset = Municipio.objects.all()
+    
+    def get_serializer_class(self):
+        if self.action == 'retrieve' or self.action == 'list':
+            return MunicipioDetailSerializer
+        return MunicipioSerializer
--- sinergia/sinergia/settings.py
+++ sinergia/sinergia/settings.py
@@ -37,8 +37,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'generales',
 ]
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        # 'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -112,7 +121,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-us'
 
 TIME_ZONE = 'UTC'
 
